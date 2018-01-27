1792d433e122963050a6af65abbe5e8b1caf7f1b
Kevin Liu <gkliu93@msn.com>
2018-01-27 11:26:09 -0800

    add react tutorial - tictactoe

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.js';
+import Game from './TicTacToe.js'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('tic-tac-toe')
+);
+
 registerServiceWorker();
