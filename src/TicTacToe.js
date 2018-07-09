5c000eef85d752fc4fa9ace8a884d7bbc997158c
g-k-l <kevin.liu@simplelegal.com>
2018-07-09 08:37:32 -0700

    clean up styling. added resetting and undoing visual elements for game workflow (behavior not completed tho)

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,18 +1,15 @@
-import React from 'react'
-import './TicTacToe.css'
-import {calculateWinner, range} from './utils.js'
-import SquareRenderer from './assets/renderUtils.js'
-import {MAX_BOARD_SIZE} from './constants.js'
+import React from "react";
+import "./TicTacToe.css";
+import { calculateWinner, range } from "./utils.js";
+import SquareRenderer from "./assets/renderUtils.js";
+import { MAX_BOARD_SIZE } from "./constants.js";
 
 class Marker extends React.Component {
   render() {
-
-      if (!this.props.src) {
-        return null
-      }
-      return (
-        <img className="marker" src={this.props.src}></img>
-      );
+    if (!this.props.src) {
+      return null;
+    }
+    return <img className="marker" src={this.props.src} />;
   }
 }
 
@@ -20,7 +17,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square" onClick={() => this.props.onClick()}>
-      <Marker src={this.props.value}/>
+        <Marker src={this.props.value} />
       </button>
     );
   }
@@ -31,7 +28,7 @@ class Board extends React.Component {
     super(props);
     this.state = {
       size: this.props.size,
-      squares: Array(this.props.size*this.props.size).fill(null),
+      squares: Array(this.props.size * this.props.size).fill(null),
       xIsNext: true,
       winner: null
     };
@@ -40,7 +37,7 @@ class Board extends React.Component {
   renderRow(rowNumber) {
     let rowRangeArr = range(this.state.size);
     console.log(this.state.size);
-        console.log(rowRangeArr);
+    console.log(rowRangeArr);
     return (
       <div className="boardRow">
         {rowRangeArr.map(colNumber => this.renderSquare(rowNumber, colNumber))}
@@ -49,9 +46,9 @@ class Board extends React.Component {
   }
 
   renderSquare(rowNumber, colNumber) {
-    const squareNumber = rowNumber*this.state.size + colNumber
+    const squareNumber = rowNumber * this.state.size + colNumber;
     return (
-      <Square 
+      <Square
         value={SquareRenderer.asXO(this.state.squares[squareNumber])}
         onClick={() => this.handleClick(squareNumber)}
       />
@@ -59,25 +56,25 @@ class Board extends React.Component {
   }
 
   currentTurn() {
-    return this.state.xIsNext ? 'X' : 'O';
+    return this.state.xIsNext ? "X" : "O";
   }
 
   handleClick(i) {
     if (this.state.winner) {
-      alert("Game is over!")
-      return
+      alert("Game is over!");
+      return;
     }
 
     const squares = this.state.squares.slice();
     if (this.state.squares[i] === null) {
-      squares[i] = this.currentTurn();  
+      squares[i] = this.currentTurn();
     } else {
-      alert("Square is taken =(")
-      return
+      alert("Square is taken =(");
+      return;
     }
 
-    const winner = calculateWinner(squares)
-    
+    const winner = calculateWinner(squares);
+
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext,
@@ -90,7 +87,7 @@ class Board extends React.Component {
     if (this.state.winner) {
       status = "Winner: ".concat(this.state.winner);
     } else {
-      status = 'Next player: '.concat(this.currentTurn());
+      status = "Next player: ".concat(this.currentTurn());
     }
     let rangeArr = range(this.state.size);
     console.log(rangeArr);
@@ -103,28 +100,41 @@ class Board extends React.Component {
   }
 }
 
+class ResetButton extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  // render() {
+
+  // }
+}
 
 class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       size: this.props.size
-    }
+    };
   }
   render() {
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board size={this.state.size}/>
+      <div>
+        <div className="game">
+          <div className="game-board">
+            <Board size={this.state.size} />
+          </div>
+        </div>
+        <div className="game-options">
+          <input className="button" type="submit" value="Undo" />
+          <input className="button" type="submit" value="Restart" />
         </div>
       </div>
     );
   }
 }
 
-
 class GameSetup extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -136,49 +146,62 @@ class GameSetup extends React.Component {
   }
 
   handleChange(event) {
-    this.setState({size: event.target.value});
+    this.setState({ size: event.target.value });
   }
 
   handleSubmit(event) {
-    const input_size = parseInt(this.state.size)
+    const input_size = parseInt(this.state.size);
 
     if (isNaN(input_size)) {
-      alert("Please enter a number >:(")
+      alert("Please enter a number >:(");
       event.preventDefault();
-      return
+      return;
     }
-    
+
     if (input_size > MAX_BOARD_SIZE) {
-      alert("Board cannot be larger than " 
-        + MAX_BOARD_SIZE + " by " + MAX_BOARD_SIZE)
+      alert(
+        "Board cannot be larger than " +
+          MAX_BOARD_SIZE +
+          " by " +
+          MAX_BOARD_SIZE
+      );
       event.preventDefault();
-      return
+      return;
     }
-    
-    this.setState({started: true, size: input_size})
+
+    this.setState({ started: true, size: input_size });
     event.preventDefault();
   }
 
   render() {
     if (this.state.started) {
-      return <Game size={this.state.size}/>
+      return <Game size={this.state.size} />;
     } else {
       return (
         <div className="game-setup">
-        How big is the board?
+          How big is the board?
           <form onSubmit={this.handleSubmit}>
             <div className="board-size">
-              <input className="text-box" type="text" value={this.state.size} onChange={this.handleChange}/>
+              <input
+                className="text-box"
+                type="text"
+                value={this.state.size}
+                onChange={this.handleChange}
+              />
               by
-              <input className="text-box" type="text" value={this.state.size} onChange={this.handleChange}/>
+              <input
+                className="text-box"
+                type="text"
+                value={this.state.size}
+                onChange={this.handleChange}
+              />
             </div>
-            <input className="button" type="submit" value="Submit"/>
+            <input className="button" type="submit" value="Submit" />
           </form>
         </div>
-      );  
+      );
     }
   }
 }
 
 export default GameSetup;
-
