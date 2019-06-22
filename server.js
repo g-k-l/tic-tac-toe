48a47250e52d6724db65ea26c97195f95b18de57
Kevin Liu <gkliu93@msn.com>
2019-06-22 16:59:44 -0700

    working development setup for both react SPA and express server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8888
+const port = 3001 
 
 app.get("/", (req, res) => res.send("hello world"))
-app.listen(port, () => console.log("Working!"))
+app.listen(port, () => console.log("Wat!"))
