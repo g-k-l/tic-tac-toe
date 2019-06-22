6541e68fcf61d21145db0870ac07895c15ad6ae2
Kevin Liu <gkliu93@msn.com>
2019-06-22 15:00:35 -0700

    move TicTacToe into its own directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import GameSetup from './TicTacToe.js'
+import GameSetup from './TicTacToe/TicTacToe.js'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
