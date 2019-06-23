3fb5bb5e8dafabc430fb0b663119c3a5715fa4b6
Kevin Liu <gkliu93@msn.com>
2019-06-22 18:33:49 -0700

    resolve incompatible eslint issue

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = 3001;
 app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get("/*", (req, res) => {
+  res.set('Cache-Control', 'no-store');
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 app.listen(port, () => console.log("Wat!"));
