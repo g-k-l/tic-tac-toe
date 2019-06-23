30aeb01742e293a3439aeecaab9c8592117bd574
Kevin Liu <gkliu93@msn.com>
2019-06-22 19:34:35 -0700

    test api endpoint working

--- server.js
+++ server.js
@@ -3,10 +3,31 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.get("/*", (req, res) => {
-  res.set('Cache-Control', 'no-store');
+APIRouter = express.Router();
+
+// API-related routes
+APIRouter.route("/test")
+  .get((req, res) => {
+    res.send("Hello, API");
+  });
+
+app.use("/api/v1", APIRouter);
+
+
+// for all non-API routes, serve the react app static
+app.use(
+  express.static(path.join(__dirname, "client", "build"), {
+    fallthrough: false
+  })
+);
+
+FallthroughRouter = express.Router()
+FallthroughRouter.get("/*", (req, res) => {
+  res.set("Cache-Control", "no-store");
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-app.listen(port, () => console.log("Wat!"));
+app.use(FallthroughRouter);
+
+
+app.listen(port, () => console.log("Server is running..."));
